7a2644c4b6b43006e9d3ec4e25c3feb5aaddea57
Shougot mollik <shougotmollik@gmail.com>
2024-04-19 00:03:13 +0600

    color

---
+++ lib/Utils/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+//  Common Colors Strings
+  static const primaryColor = Color(0xffF4511E);
+  static const onPrimaryColor = Color(0xff000000);
+  static const secondaryColor = Color(0xff2c2f38);
+  static const whiteColor = Color(0xffffffff);
+  static const redColor = Color(0xffE53935);
+  static const greenColor = Color(0xff00C853);
+
+//   Light Theme Colors
+  static const lightBackgroundColor = Color(0xffFBE9E7);
+}
