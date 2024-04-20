0fbb2f48e71758a8bbdc1e181893cbeae2bfc2c5
Shougot mollik <shougotmollik@gmail.com>
2024-04-20 22:57:49 +0600

    color

--- lib/Utils/colors.dart
+++ lib/Utils/colors.dart
@@ -6,7 +6,7 @@ class AppColors {
 //  Common Colors Strings
   static const primaryColor = Color(0xffF4511E);
   static const onPrimaryColor = Color(0xff000000);
-  static const secondaryColor = Color(0xff2c2f38);
+  static const secondaryColor = Color(0xff424242);
   static const whiteColor = Color(0xffffffff);
   static const redColor = Color(0xffE53935);
   static const greenColor = Color(0xff00C853);
