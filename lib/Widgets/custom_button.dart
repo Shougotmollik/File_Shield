6c2e0708bca652ca9953f6421d81580ec27c8e3f
Shougot mollik <shougotmollik@gmail.com>
2024-04-20 22:58:26 +0600

    custom button

---
+++ lib/Widgets/custom_button.dart
@@ -0,0 +1,41 @@
+import 'package:file_shield/Utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      required this.btnText,
+      this.onTap,
+      this.backgroundColor,
+      this.textColor});
+
+  final String btnText;
+  final void Function()? onTap;
+  final Color? backgroundColor;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+        decoration: BoxDecoration(
+          color: backgroundColor ?? Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: Center(
+          child: Text(
+            btnText,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              color: textColor ?? Theme.of(context).colorScheme.onPrimary,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
