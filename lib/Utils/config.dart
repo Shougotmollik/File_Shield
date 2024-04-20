8dbb0d518dc6e3450061eb423168cccd4bfd6caf
Shougot mollik <shougotmollik@gmail.com>
2024-04-20 22:58:04 +0600

    config

--- lib/Utils/config.dart
+++ lib/Utils/config.dart
@@ -2,4 +2,25 @@ class AppConfig {
   AppConfig._();
 
   static const appName = "File Shield";
+  static const privacyText = """
+  1.Permissions
+    we may ask you to provide certain permissions in     
+    order to let our features perform properly.
+    
+    In order to provide hiding,photos,videos and other's 
+    file.We are require the permission(s):File read/write. 
+                  
+    To snap intruder photos after invalid attempt we are
+    require to permission(s):camera.  
+    
+                 
+  2.Privacy
+    The following information is only used to improve the
+    product experience and will not be sold any third
+    parties.
+    
+    we will not use this information for purpose other
+    than improving the product experience and
+    performances.
+                """;
 }
