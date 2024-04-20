e6f8f62ba0ca37408a91803b4a27ea6c13d628a0
Shougot mollik <shougotmollik@gmail.com>
2024-04-21 00:28:35 +0600

    app bar

---
+++ lib/Widgets/custom_app_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppbar({super.key, this.title, this.isLeading, this.actions});
+
+  final String? title;
+  final bool? isLeading;
+  final List<Widget>? actions;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Text(
+        title ?? "",
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
+      ),
+      automaticallyImplyLeading: false,
+      leading: isLeading == true
+          ? IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_back,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            )
+          : null,
+      actions: actions ??
+          [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.settings,
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ))
+          ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
