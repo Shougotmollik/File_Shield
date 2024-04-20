ced0ba18ad58c9f760d14b10263d41650264d894
Shougot mollik <shougotmollik@gmail.com>
2024-04-20 22:59:29 +0600

    splash screen

--- lib/Pages/SplashPage/splash_screen.dart
+++ lib/Pages/SplashPage/splash_screen.dart
@@ -1,10 +1,48 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:file_shield/Pages/WelcomePage/welcome_screen.dart';
+import 'package:file_shield/Utils/config.dart';
+import 'package:file_shield/Utils/images.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Get.to(() => const WelcomeScreen());
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(AppImages.splashScreenImage),
+            SizedBox(height: MediaQuery.of(context).size.height * .03),
+            Text(
+              AppConfig.appName,
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.italic,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
