488900d4f0bdf1020112aa0dd8f2a1f95db50095
Shougot mollik <shougotmollik@gmail.com>
2024-04-20 22:58:38 +0600

    image

---
+++ lib/Utils/images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  AppImages._();
+
+//   All Images here
+
+  static const splashScreenImage =
+      "assets/images/secure-lock-and-key-successfully-unlocked.gif";
+
+  static const welcomeScreenImage = "assets/images/welcomeScreen.png";
+}
