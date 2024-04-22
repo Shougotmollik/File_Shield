177c29472c2f2e49f8ac10800c5dbf43b9f2ba78
Shougot mollik <shougotmollik@gmail.com>
2024-04-22 22:09:02 +0600

    calculator Screen

--- lib/Screens/CalculatorScreen/calculator_screen.dart
+++ lib/Screens/CalculatorScreen/calculator_screen.dart
@@ -1,4 +1,5 @@
 import 'package:file_shield/Widgets/custom_app_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorScreen extends StatefulWidget {
@@ -11,8 +12,81 @@ class CalculatorScreen extends StatefulWidget {
 class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppbar(),
+    return Scaffold(
+      appBar: const CustomAppbar(),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Please enter your passcode",
+                style: TextStyle(
+                  color:
+                      Theme.of(context).colorScheme.onPrimary.withOpacity(0.5),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        "500",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                          fontSize: 35,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        "500",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                          fontSize: 40,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              GridView.builder(
+                itemCount: 10,
+                primary: false,
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 12.0,
+                  crossAxisSpacing: 12.0,
+                ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(16.0),
+                        border: Border(
+                          top: BorderSide(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            width: 1.5,
+                          ),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            spreadRadius: .075,
+                            blurRadius: 0.5,
+                          )
+                        ]),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
