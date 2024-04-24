363f75830c1a4e10c8151092fcfa7246ab782bf5
Shougot mollik <shougotmollik@gmail.com>
2024-04-24 23:48:45 +0600

    calculator screen

--- lib/Screens/CalculatorScreen/calculator_screen.dart
+++ lib/Screens/CalculatorScreen/calculator_screen.dart
@@ -1,6 +1,7 @@
+import 'package:file_shield/Controllers/calculator_controller.dart';
 import 'package:file_shield/Widgets/custom_app_bar.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
@@ -13,80 +14,106 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppbar(),
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Please enter your passcode",
-                style: TextStyle(
-                  color:
-                      Theme.of(context).colorScheme.onPrimary.withOpacity(0.5),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        "500",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onPrimary,
-                          fontSize: 35,
-                          fontWeight: FontWeight.w800,
-                        ),
+        appBar: const CustomAppbar(),
+        body: GetBuilder<CalculatorController>(
+          builder: (controller) {
+            return Padding(
+              padding: const EdgeInsets.all(12),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Please enter your passcode",
+                      style: TextStyle(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .onPrimary
+                            .withOpacity(0.8),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
                       ),
-                      Text(
-                        "500",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onPrimary,
-                          fontSize: 40,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              GridView.builder(
-                itemCount: 10,
-                primary: false,
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  mainAxisSpacing: 12.0,
-                  crossAxisSpacing: 12.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "500",
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                                fontSize: 35,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            Text(
+                              "500",
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                                fontSize: 40,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    GridView.builder(
+                      itemCount: controller.button.length,
+                      primary: false,
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 12.0,
+                        crossAxisSpacing: 12.0,
+                      ),
+                      itemBuilder: (context, index) {
+                        final button = controller.button[index];
+                        return button.isEmpty
+                            ? const SizedBox()
+                            : Container(
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .primary
+                                        .withOpacity(0.9),
+                                    borderRadius: BorderRadius.circular(16.0),
+                                    border: Border(
+                                      top: BorderSide(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary,
+                                        width: 1.5,
+                                      ),
+                                    ),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary,
+                                        spreadRadius: .075,
+                                        blurRadius: 0.5,
+                                      )
+                                    ]),
+                                child: Center(
+                                    child: Text(
+                                  button,
+                                  style: const TextStyle(
+                                    fontSize: 38,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                              );
+                      },
+                    )
+                  ],
                 ),
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: BorderRadius.circular(16.0),
-                        border: Border(
-                          top: BorderSide(
-                            color: Theme.of(context).colorScheme.onPrimary,
-                            width: 1.5,
-                          ),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Theme.of(context).colorScheme.onPrimary,
-                            spreadRadius: .075,
-                            blurRadius: 0.5,
-                          )
-                        ]),
-                  );
-                },
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+              ),
+            );
+          },
+        ));
   }
 }
