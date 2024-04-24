127990c62c81a08a5cefc2edb03508a3a2d911d2
Shougot mollik <shougotmollik@gmail.com>
2024-04-24 23:48:41 +0600

    calculator screen

--- lib/Controllers/calculator_controller.dart
+++ lib/Controllers/calculator_controller.dart
@@ -1,3 +1,26 @@
 import 'package:get/get.dart';
 
-class CalculatorController extends GetxController {}
+class CalculatorController extends GetxController {
+  List<String> button = [
+    '',
+    '',
+    'AC',
+    'C',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    '*',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '.',
+    '=',
+    '+',
+  ];
+}
